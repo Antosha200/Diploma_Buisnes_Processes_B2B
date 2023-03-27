b5097c01a5552a2ca28d769004217a67e40a1618
anton <anton2-2000@mail.ru>
2023-03-27 14:03:52 +0300

    dev adding new InventoryManagement module

---
+++ app/code/NaumovExtensions/InventoryManagement/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @category NaumovExtensions
+ * @package NaumovExtensions_InventoryManagement
+ * @author Anton Naumov <anton2-2000@mail.ru>
+ * @copyright Copyright (c) 2023 NaumovExtensions
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'NaumovExtensions_InventoryManagement',
+    __DIR__
+);
